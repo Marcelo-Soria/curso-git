4c0fc726c23cacffac3011d4b45bdd6a97b45221
Marcelo Soria <msoria.rodrigues@gmail.com>
2021-07-08 14:24:44 -0300

    Acrescentada uma msg Como voce esta

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -8,6 +8,8 @@ namespace projeto_teste
         {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está?");
+
         }
     }
 }
